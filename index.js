c6dd2d45092360b7e65fe58c487331558b1b8bfe
SAMUEL KOBINA GYASI <gyasisamuelkobina22@gmail.com>
2022-08-16 17:51:42 +0100

    churchsite1

---
+++ index.js
@@ -0,0 +1,9 @@
+var mobileMenu = document.getElementById('mobile-menu');
+
+function openMobileMenu() {
+    mobileMenu.style.right = '0';
+}
+
+function closeMobileMenu() {
+    mobileMenu.style.right = '-200px';
+}
